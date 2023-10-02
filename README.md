68ffff9492482fc6d1520ccfc327b3e50e02b0c1
Xabier Lameiro <xabierlca@ext.inditex.com>
2023-10-02 20:04:54 +0200

    New content

---
+++ README.md
@@ -0,0 +1,8 @@
+# Y.js demo-server for Ogma collaboration example
+
+All yjs-demos connect to this server to exchange document updates. It combines the y-websocket server with an http server for the files in the root directory. Of course, you can choose any of the Yjs providers instead. This is just a demo server.
+
+```sh
+npm install
+npm start
+```
---
+++ index.js
@@ -0,0 +1,32 @@
+/**
+ * @type {any}
+ */
+const WebSocket = require("ws");
+const http = require("http");
+const StaticServer = require("node-static").Server;
+const setupWSConnection = require("y-websocket/bin/utils").setupWSConnection;
+
+const production = process.env.PRODUCTION != null;
+const port = process.env.PORT || 8080;
+
+const staticServer = new StaticServer("../", {
+  cache: production ? 3600 : false,
+  gzip: production,
+});
+
+const server = http.createServer((request, response) => {
+  request
+    .addListener("end", () => {
+      staticServer.serve(request, response);
+    })
+    .resume();
+});
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (conn, req) => setupWSConnection(conn, req, {}));
+
+server.listen(port);
+
+console.log(
+  `Listening to http://localhost:${port} ${production ? "(production)" : ""}`
+);
