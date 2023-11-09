1c8fd603f2abf1bfaae85c7c00faea5e01cc6ffe
Xabier Lameiro <xabierlca@ext.inditex.com>
2023-11-09 18:26:34 +0100

    Test

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ const server = http.createServer((request, response) => {
 const wss = new WebSocket.Server({ server });
 
 wss.on("connection", (conn, req) => {
-  console.log(`conn: ${conn}`);
-  console.log(`req: ${req}`);
+  console.log(`conn: ${{conn}}`);
+  console.log(`req: ${{req}}`);
   return setupWSConnection(conn, req, {});
 });
 
